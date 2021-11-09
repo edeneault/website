6f22fa559f27f77ee4fab6b267128a01b02f5c16
edeneault <edeneault@gmail.com>
2021-11-09 11:17:50 -0800

    Update: pull-request-trigger.yml-sddcomment and linked issue

--- github-actions/trigger-pr/trigger-pr.js
+++ github-actions/trigger-pr/trigger-pr.js
@@ -17,12 +17,12 @@ async function main({ g, c }) {
   github = g;
   context = c;
   console.log(context.payload.number, context.payload.repository.owner.login);
-  console.log(github);
-  for (const item in github.request) {
+  // console.log(github);
+  for (const item in github.rest) {
     console.log(item);
   }
 
-  const results = await github.projects.createCard(
+  const results = await github.rest.projects.createCard(
     (content_id = context.payload.number),
     (content_type = "PullRequest"),
     (column = "PR Needs Review"),
