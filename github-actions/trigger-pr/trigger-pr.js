c9049de0ca8c943ac59ae24f9423389491db11fa
edeneault <edeneault@gmail.com>
2021-11-09 06:15:08 -0800

    Update: pull-request-trigger.yml-sddcomment and linked issue

---
+++ github-actions/trigger-pr/trigger-pr.js
@@ -0,0 +1,33 @@
+// Constant variables
+// const REQUIRED_LABELS = ['Size', 'role', 'Feature']
+// const LABEL_MISSING = ['size: missing', 'role missing', 'Feature Missing']
+// const SIZE_EXCEPTIONS = ['good first issue']
+
+// Global variables
+var github;
+var context;
+
+/**
+ * Check the labels of an issue, and add/remove labels as necessary
+ * @param {Object} g - github object
+ * @param {Object} c - context object
+ * @returns - returns an object with the action's result, which is passed on to the next action
+ */
+async function main({ g, c }) {
+  github = g;
+  context = c;
+  console.log(context, github);
+  // const issueNum = context.payload.issue.number
+
+  // const labels = obtainLabels()
+  // const filteredLabels = filterLabels(labels)
+  // const labelsToAdd = checkLabels(filteredLabels)
+  // console.log('Labels to add: ', labelsToAdd)
+
+  // const result = await addLabels(labelsToAdd, filteredLabels)
+  // return {
+  //   actionResult: result,
+  //   addedLabels: labelsToAdd,
+  //   issueNum: issueNum
+  // }
+}
