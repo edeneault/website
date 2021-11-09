b038578f8eaa2c48c97c259cda9a9629e9963c5f
edeneault <edeneault@gmail.com>
2021-11-09 08:52:46 -0800

    Update: pull-request-trigger.yml-sddcomment and linked issue

--- github-actions/trigger-pr/trigger-pr.js
+++ github-actions/trigger-pr/trigger-pr.js
@@ -16,7 +16,7 @@ var context;
 async function main({ g, c }) {
   github = g;
   context = c;
-  console.log(context.payload.pull_request.links.issue, github);
+  console.log(context.payload.pull_request.links, github);
   // const issueNum = context.payload.issue.number
 
   // const labels = obtainLabels()
