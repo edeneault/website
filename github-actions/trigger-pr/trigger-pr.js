5aa3efa36e33101c7d5c1a18c343620b8cf2e046
edeneault <edeneault@gmail.com>
2021-11-09 11:50:41 -0800

    Update: pull-request-trigger.yml-sddcomment and linked issue

--- github-actions/trigger-pr/trigger-pr.js
+++ github-actions/trigger-pr/trigger-pr.js
@@ -25,13 +25,13 @@ async function main({ g, c }) {
   const columns = await github.rest.projects.listColumns();
   console.log(columns);
 
-  const results = await github.rest.projects.createCard(
-    (content_id = context.payload.number),
-    (content_type = "PullRequest"),
-    (column = "PR Needs Review"),
-    (project = "Project Board"),
-    (user = context.payload.repository.owner.login),
-  );
+  // const results = await github.rest.projects.createCard(
+  //   (content_id = context.payload.number),
+  //   (content_type = "PullRequest"),
+  //   (column = "PR Needs Review"),
+  //   (project = "Project Board"),
+  //   (user = context.payload.repository.owner.login),
+  // );
 }
 
 module.exports = main;
